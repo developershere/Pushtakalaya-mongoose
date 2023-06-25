6519dfbc662b03bd5b7a6ee3725d56d47f9c5cf2
Mausam <techdevelopers@gamil.com>
2023-06-25 10:13:46 -0700

    Disable auth

--- routes/order.route.js
+++ routes/order.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/saveorder",saveOrder);
 router.get("/vieworder",vieworderList)
 router.post("/vieworderByorderId",vieworderByorderId)
-router.post("/vieworderByuserId",verifyToken, vieworderHistoryByUserId);
+router.post("/vieworderByuserId", vieworderHistoryByUserId);
 router.put("/changestatus/:orderId",changestatus);
 router.post("/viewproductBySellerId",viewOrderBySellerId)
 
